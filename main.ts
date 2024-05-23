7dc4b4ff124e8a0c27c3920188ce8be6000029e2
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 22:51:56 +0500

    twenty assignment

---
+++ main.ts
@@ -0,0 +1,16 @@
+
+
+//Q:20 Think of something you could store in a array. For example, you could make a list of mountains, 
+// rivers, countries, cities, languages, or anything else you’d like. 
+// Write a program that creates a list containing these items.
+
+const Countries: string[] =["London","UAE","Saudi arab","Paris"];
+
+console.log("\nlist of Countries:");
+for(let a =0; a < Countries.length; a++){
+console.log (Countries[a]);
+}
+
+
+
+
